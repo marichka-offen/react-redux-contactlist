9d15171df38eb0ecaad2578087f515eaaae8f19e
Marichka Tsiuriak <marichka.offen@gmail.com>
2019-03-14 02:15:15 -0400

    Create Contacts and Contact cmpnts Create mock-up state in Components, pass it through props to Component; Use React.Fragment for cpmnt children; Use map to iterate through state to pass each contact as a prop to Contact cpmnt; Create and style Contact cmpnt to render values of each contact; Create edit and delete buttons in Contact cmpnt; Import Contacts tp App.js.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
+import Contacts from "./components/contacts/Contacts";
+import EditContact from "./components/contacts/Contact";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,7 +16,9 @@ class App extends Component {
         <Header branding="Contact Manager" />
         <div className="container">
           <Switch>
+            <Route exact path="/" component={Contacts} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/contact/edit/:id" component={EditContact} />
             <Route component={NotFound} />
           </Switch>
         </div>
---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  handleDelete = id => {
+    // DELETE CONTACT
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down ml-1"
+            style={{ cursor: "pointer" }}
+          />
+
+          <div style={{ float: "right" }}>
+            <i className="fas fa-pencil-alt p-2">
+              <Link to={`/contact/edit/${id}`} />
+            </i>
+            <i
+              className="fas fa-times text-danger"
+              onClick={this.handleDelete.bind(this, id)}
+            />
+          </div>
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Contact;
