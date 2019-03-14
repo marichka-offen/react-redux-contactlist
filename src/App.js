2efb97554f084d8a3dc87c71e762775d1643ea1c
Marichka Tsiuriak <marichka.offen@gmail.com>
2019-03-14 01:37:23 -0400

    Create About and NotFound rfc Import them to App.js, set routes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/layout/Header";
+import NotFound from "./components/pages/NotFound";
+import About from "./components/pages/About";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,6 +12,12 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
+        <div className="container">
+          <Switch>
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
     );
   }
